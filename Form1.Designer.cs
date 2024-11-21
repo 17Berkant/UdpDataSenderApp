d5cf9bda693fc80a5888b90963123dfaeeb08bcf
berkantakyildiz <berkantakyildiz@gmail.com>
2024-11-21 11:44:37 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace UdpDataSenderApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ipTextBox = new System.Windows.Forms.TextBox();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.dataTextBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ipTextBox
+            // 
+            this.ipTextBox.Location = new System.Drawing.Point(45, 28);
+            this.ipTextBox.Name = "ipTextBox";
+            this.ipTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ipTextBox.TabIndex = 0;
+            this.ipTextBox.Text = "127.0.0.1";
+            // 
+            // portTextBox
+            // 
+            this.portTextBox.Location = new System.Drawing.Point(45, 54);
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(100, 20);
+            this.portTextBox.TabIndex = 1;
+            this.portTextBox.Text = "12001";
+            // 
+            // dataTextBox
+            // 
+            this.dataTextBox.Location = new System.Drawing.Point(45, 80);
+            this.dataTextBox.Name = "dataTextBox";
+            this.dataTextBox.Size = new System.Drawing.Size(100, 20);
+            this.dataTextBox.TabIndex = 2;
+            this.dataTextBox.Text = "2^COCA COLA ZERO^100.00^M^100.00^False^104019";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(45, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Gonder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(195, 173);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataTextBox);
+            this.Controls.Add(this.portTextBox);
+            this.Controls.Add(this.ipTextBox);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox ipTextBox;
+        private System.Windows.Forms.TextBox portTextBox;
+        private System.Windows.Forms.TextBox dataTextBox;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace UdpDataSenderApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            // Kullanıcıdan IP, port ve mesaj bilgilerini al
+            string ipAddress = ipTextBox.Text;
+            int port = int.Parse(portTextBox.Text);
+            string dataToSend = dataTextBox.Text;
+
+            using (UdpClient udpClient = new UdpClient())
+            {
+                try
+                {
+                    // IP adresi ve port ile endpoint oluştur
+                    IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(ipAddress), port);
+
+                    // Mesajı byte array'e dönüştür
+                    byte[] sendData = Encoding.UTF8.GetBytes(dataToSend);
+
+                    // Veriyi gönder
+                    await udpClient.SendAsync(sendData, sendData.Length, endPoint);
+
+                }
+                catch (Exception ex)
+                {
+                    // Hata mesajını göster
+                    MessageBox.Show($"Veri gönderme sırasında hata oluştu: {ex.Message}");
+                }
+            }
+
+        }
+    }
+}
